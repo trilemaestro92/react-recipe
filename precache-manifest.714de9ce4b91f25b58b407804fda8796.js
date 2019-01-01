d2ce942c88ebe46626ac0725a9b9adeb359dbbda
Tri Nguyen <trihuynguyen14@gmail.com>
2019-01-01 16:12:33 -0500

    Updates

---
+++ precache-manifest.714de9ce4b91f25b58b407804fda8796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94cef531455476599275",
+    "url": "/react-recipe/static/js/runtime~main.94cef531.js"
+  },
+  {
+    "revision": "f6465227eb9db8b3f2bf",
+    "url": "/react-recipe/static/js/main.f6465227.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/react-recipe/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "f6465227eb9db8b3f2bf",
+    "url": "/react-recipe/static/css/main.2c526d12.chunk.css"
+  },
+  {
+    "revision": "30bf47bd00c98d2a5fc6b065f4ff357a",
+    "url": "/react-recipe/index.html"
+  }
+];
